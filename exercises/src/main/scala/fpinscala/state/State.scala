ac7b485bbbfe98c042e0fbeeba5e5b5c7ce3627b
steve.etherington <setherington@equalexperts.com>
2015-09-27 10:42:38 +0100

    implement some higher order functions for State, and some for parallelism

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -147,7 +147,6 @@ case class State[S, +A](run: S => (A, S)) {
       f(a).run(s2)
     })
 
-
 }
 
 sealed trait Input
@@ -162,5 +161,24 @@ object State {
   type Rand[A] = State[RNG, A]
   def unit[S,A](a: A): State[S,A] = State({s => (a, s)})
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+   sequence(inputs.map(i => modify((s: Machine) => (i,s) match {
+     case (_,Machine(_,0,_)) => s
+     case (Coin,Machine(true,candies,coins)) => Machine(false,candies,coins+1)
+   }
+   ))).flatMap{ _ => get.map{ m => (m.coins,m.candies)}}
 }
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,6 +1,7 @@
 package fpinscala.testing
 
 import fpinscala.laziness.Stream
+import fpinscala.state.RNG.{Rand, Simple}
 import fpinscala.state._
 import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
@@ -20,8 +21,16 @@ object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
+
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  case class Gen[A](sample: State[RNG,A])
+  def unit[A](a: => A): Gen[A] = Gen(State(RNG.unit(a)))
+  def boolean: Gen[Boolean] = Gen(State(RNG.int).map(_ %2 ==0))
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(State.sequence(List.fill(n)(g.sample)))
+  def choose(start:Int,stopExclusive:Int) :Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(i => (i+start) % (stopExclusive-start)))
+
+
 }
 
 trait Gen[A] {
